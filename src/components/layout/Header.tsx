3a0f83b2dbf9304afd95c180a3568c79d7aefb7e
NexoCeo <NexoCeoGroup@gmail.com>
2026-08-27 13:53:30 -0300

    Retirando importação Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,6 @@ import BrandMark from '../common/BrandMark'
 
 import { navigation } from '../data/navigation'
 import { scrollToSection } from '../../utils/scrollToSection'
-import imgLogin from '@/images/logo.png';
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false)
