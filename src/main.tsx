fe6251e5cbfc98706db9cde46e7df9e03fdd68df
NexoCeo <NexoCeoGroup@gmail.com>
2026-08-27 21:15:31 -0300

    Ajuste de espaçamento lateral

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { ThemeProvider } from './context/ThemeContext';
 
-//import '@/styles/global.css'
+import '@/styles/global.css'
 
 const rootElement =
   document.getElementById('root')
